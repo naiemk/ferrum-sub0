841bbe6220fc48650e721b173f5fc595797e9b86
nyeganeh <nyeganeh@bloomberg.net>
2018-10-02 00:59:46 -0400

    Adding ant-design-rn, fix typescript to 2.8.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Button} from 'antd-mobile-rn';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Button>
+          Start
+        </Button>
       </View>
     );
   }
